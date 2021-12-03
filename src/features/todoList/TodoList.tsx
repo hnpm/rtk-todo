93021d2201c2bd46ec54f29b10472a38af7703f6
Hien Pham <phaminhien@hotmail.com>
2021-12-03 13:11:47 +0700

    toggle todo

--- src/features/todoList/TodoList.tsx
+++ src/features/todoList/TodoList.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 import { RootState } from "app/rootReducer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTodo } from "./todoSlice";
 
 export default function TodoList() {
     const todos = useSelector(
         (state: RootState) => state.todos
     );
 
+    const dispatch = useDispatch();
+
     return (
         <ul>
             {todos.map(todo => (
-                <TodoListItem key={todo.id} {...todo} />
+                <TodoListItem key={todo.id} {...todo} onClick={() => dispatch(toggleTodo(todo))} />
             ))}
         </ul>
     );
--- src/features/todoList/TodoListItem.tsx
+++ src/features/todoList/TodoListItem.tsx
@@ -3,11 +3,12 @@ import React from "react";
 interface TodoProps {
     completed: boolean,
     text: string,
+    onClick: () => any,
 }
 
-export default function TodoListItem({ completed, text } : TodoProps) {
+export default function TodoListItem({ completed, text, onClick } : TodoProps) {
     return (
-        <li style={{textDecoration: completed ? 'line-through' : 'none'}}>
+        <li onClick={onClick} style={{textDecoration: completed ? 'line-through' : 'none'}}>
             {text}
         </li>
     )
