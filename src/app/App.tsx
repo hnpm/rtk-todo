0e82bd03ec7cad7670d8397a3e0078112ed6a32f
Hien Pham <phaminhien@hotmail.com>
2021-12-04 23:58:23 +0700

    load data from api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,22 @@
 import AddTodo from 'features/todoList/AddTodo';
 import TodoList from 'features/todoList/TodoList';
+import { createTodoList, loadTodos } from 'features/todoList/todoSlice';
 import Footer from 'features/visibilityFilter/Footer';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (window.location.pathname === '/') {
+      dispatch(createTodoList());
+    } else {
+      dispatch(loadTodos());
+    }
+  }, [dispatch]);
+  
   return (
     <div>
       <AddTodo />
--- src/features/todoList/todoSlice.ts
+++ src/features/todoList/todoSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
+import { RootState } from "app/rootReducer";
+import { writeTodos, readTodos as fetchTodos } from "api/jsonstore";
 import { AppDispatch, AppThunk } from "app/store";
 import { Todo } from "./types";
 
@@ -17,19 +18,33 @@ const todoSlice = createSlice({
             if (todo) {
                 todo.completed = !todo.completed;
             }
+        },
+        receiveTodos(state, action: PayloadAction<Todo[]>) {
+            return action.payload;
         }
     }
 });
 
 export const { toggleTodo } = todoSlice.actions;
 
-export const addTodo = (text: string): AppThunk => async (dispatch: AppDispatch) => {
+export const addTodo = (text: string): AppThunk => async (dispatch: AppDispatch, getState: () => RootState) => {
     const newTodo: Todo = {
         id: Math.random().toString(36).substr(2, 9),
         completed: false,
         text: text,
     }
     dispatch(todoSlice.actions.addTodo(newTodo));
+    writeTodos(getState().todos);
+}
+
+export const createTodoList = (): AppThunk => async (dispatch: AppDispatch) => {
+    const id = Math.random().toString(36).substr(2, 9);
+    window.history.pushState(null, document.title, `${id}`);
+}
+
+export const loadTodos = (): AppThunk => async (dispatch: AppDispatch) => {
+    const todos = await fetchTodos();
+    dispatch(todoSlice.actions.receiveTodos(todos));
 }
 
 export default todoSlice.reducer;
